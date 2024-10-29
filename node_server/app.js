d6a8d36ab7869776df505c428b2eba5851eaa20a
Your Name <dropella.dev@gmail.com>
2024-10-29 12:13:20 +0300

    export app

--- node_server/app.js
+++ node_server/app.js
@@ -63,4 +63,6 @@ mongoose
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// module.exports = app;
+
+
+module.exports = app;
